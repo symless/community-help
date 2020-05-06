cf0d141059f5e0cb1c562511ead6b596af969d99
Jamie Newbon <jamie@symless.com>
2020-05-06 17:35:07 +0100

    Initial webserver with echo and hello world requests

---
+++ Webserver/src/configuration/configuration.h
@@ -0,0 +1,30 @@
+#ifndef CONFIGURATION_H
+#define CONFIGURATION_H
+/*
+ * Intervew Service
+ * Copyright (C) 2019 Symless Ltd.
+ *
+ */
+
+/**
+ * @file configuration.h
+ * @author Jamie Newbon
+ * @date 6/5/2020
+ * @brief The class deals with all configuration settings in the service
+ * 
+ */
+
+#include <string>
+
+
+class Configuration
+{
+public:
+
+
+private:
+
+
+};
+
+#endif
