e7c23afcf372878e8bafb8b80cd172f28be13664
hiperlogic <rodrigod@hiperlogic.com.br>
2020-05-07 09:22:45 -0300

    Todos and docs

--- Webserver/src/main.cpp
+++ Webserver/src/main.cpp
@@ -29,6 +29,10 @@ int main()
         return json.dump(4);
     });
 
+    /**
+     * @brief Return the  list of (open) helps registered in the system
+     * //TODO: Code the behavior
+     */
     networking::registerAPICommand("/help_requests", [](const std::string& string)->std::string
     {
         nlohmann::json json = {};
@@ -36,6 +40,10 @@ int main()
         return json.dump(4);
     });
 
+    /**
+     * @brief Return the list of (open) assistances registered in the system
+     * //TODO: Code the behavior
+     */
     networking::registerAPICommand("/assistance_provided", [](const std::string& string)->std::string
     {
         nlohmann::json json = {};
@@ -43,13 +51,25 @@ int main()
         return json.dump(4);
     });
 
+    /**
+     * @brief Registers a help entry in the system
+     * //TODO: Implement the behavior
+     */
     networking::registerAPICommand("/help", [](const std::string& string)->std::string
     {
         nlohmann::json json = {};
         json.push_back("hello World");
         return json.dump(4);
+        // Process the string to retrieve the proper parameters
+        // retrieves or instatiates the resources to call
+        return App->help(string)
+
     });
 
+    /**
+     * @brief Registers an assistance entry in the system
+     * 
+     */
     networking::registerAPICommand("/assist", [](const std::string& string)->std::string
     {
         nlohmann::json json = {};
@@ -57,6 +77,10 @@ int main()
         return json.dump(4);
     });
 
+    /**
+     * @brief Logins
+     * //TODO: Implement the behavior
+     */
     networking::registerAPICommand("/login", [](const std::string& string)->std::string
     {
         nlohmann::json json = {};
@@ -64,6 +88,10 @@ int main()
         return json.dump(4);
     });
 
+    /**
+     * @brief 
+     * //TODO: Implement the behavior
+     */
     networking::registerAPICommand("/logout", [](const std::string& string)->std::string
     {
         nlohmann::json json = {};
@@ -71,6 +99,10 @@ int main()
         return json.dump(4);
     });
 
+    /**
+     * @brief 
+     * //TODO: Implement the behavior
+     */
     networking::registerAPICommand("/signup", [](const std::string& string)->std::string
     {
         nlohmann::json json = {};
@@ -78,6 +110,10 @@ int main()
         return json.dump(4);
     });
 
+    /**
+     * @brief 
+     * 
+     */
     networking::registerAPICommand("/accept_help", [](const std::string& string)->std::string
     {
         nlohmann::json json = {};
